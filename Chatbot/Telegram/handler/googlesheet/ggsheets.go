b01a9998637f34dd7e2d71d171ed22e812743054
Phan Phu Thanh <thanhphanphu18@gmail.com>
2020-04-04 11:42:13 +0700

    telegram chatbot update

---
+++ Chatbot/Telegram/handler/googlesheet/ggsheets.go
@@ -0,0 +1,3 @@
+package googlesheet
+
+// https://developers.google.com/sheets/api/reference/rest/v4/spreadsheets.values/get?apix_params=%7B%22spreadsheetId%22%3A%221alTTN3sIuK-d-M16lV6gPK4M6AwBkc7UQj2nA5ZBol0%22%2C%22range%22%3A%22T3653%3AT3663%22%7D
---
+++ Chatbot/Telegram/handler/googlesheet/ggsheettoken.go
@@ -0,0 +1,4 @@
+package googlesheet
+
+// Get new and refresh token from google sheet
+// https://developers.google.com/sheets/api/quickstart/go#step_3_set_up_the_sample
