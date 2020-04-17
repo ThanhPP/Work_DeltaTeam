c265c2cc65b28991212348f7597ce4ae67dd515c
THANHPP <thanhphanphu18@gmail.com>
2020-04-17 17:08:30 +0700

    add concurrency to name.com handler

--- Chatbot/Telegram/handler/namedotcom/NameDotComHandler.go
+++ Chatbot/Telegram/handler/namedotcom/NameDotComHandler.go
@@ -5,6 +5,7 @@ import (
 	"log"
 	"net/http"
 	"strings"
+	"sync"
 
 	"github.com/THANHPP/Work_DeltaTeam/Chatbot/Telegram/config"
 	ggs "github.com/THANHPP/Work_DeltaTeam/Chatbot/Telegram/handler/googlesheett"
@@ -72,17 +73,50 @@ func ForwardLinks(inputRange string) (forwardResult []string, successForwardCoun
 	if err != nil {
 		log.Printf("\n\t ForwardLinks : %+v \n", err)
 	}
-
 	//Column assign
 	storeLinksCol := "T"
 	tempForwardLinksCol := "U"
 
-	//Parse range
-	storeLinks := ggs.GetDataFromRage(ggs.NewRange(firstNum, secondNum, storeLinksCol))
-	tempForwardLinks := ggs.GetDataFromRage(ggs.NewRange(firstNum, secondNum, tempForwardLinksCol))
+	//CONCURRENCY
+	//Variable
+	var wg sync.WaitGroup
+
+	var forwardResult1 []string
+	var successForwardCount1 int
+	var errorForwardCount1 int
+
+	var forwardResult2 []string
+	var successForwardCount2 int
+	var errorForwardCount2 int
+	//Concurrent func
+	wg.Add(2)
+
+	go func() {
+		fmt.Println(firstNum, int(secondNum-((secondNum-firstNum)/2)))
+		storeLinks1 := ggs.GetDataFromRage(ggs.NewRange(firstNum, int(secondNum-((secondNum-firstNum)/2)), storeLinksCol))
+		tempForwardLinks1 := ggs.GetDataFromRage(ggs.NewRange(firstNum, int(secondNum-((secondNum-firstNum)/2)), tempForwardLinksCol))
+
+		forwardResult1, successForwardCount1, errorForwardCount1 = createForwardLinks(storeLinks1, tempForwardLinks1)
+
+		wg.Done()
+	}()
+
+	go func() {
+		fmt.Println(int(secondNum-((secondNum-firstNum)/2)+1), secondNum)
+		storeLinks2 := ggs.GetDataFromRage(ggs.NewRange(int(secondNum-((secondNum-firstNum)/2)+1), secondNum, storeLinksCol))
+		tempForwardLinks2 := ggs.GetDataFromRage(ggs.NewRange(int(secondNum-((secondNum-firstNum)/2)+1), secondNum, tempForwardLinksCol))
+
+		forwardResult2, successForwardCount2, errorForwardCount2 = createForwardLinks(storeLinks2, tempForwardLinks2)
+
+		wg.Done()
+	}()
+
+	wg.Wait()
 
 	//Main phase
-	forwardResult, successForwardCount, errorForwardCount = createForwardLinks(storeLinks, tempForwardLinks)
+	forwardResult = append(forwardResult1, forwardResult2...)
+	successForwardCount = successForwardCount1 + successForwardCount2
+	errorForwardCount = errorForwardCount1 + errorForwardCount2
 
 	return forwardResult, successForwardCount, errorForwardCount
 }
